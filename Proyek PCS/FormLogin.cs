fb0a271e1e060e8dfee0557c3aac7d4b63c6150b
Edwinlo98 <48885699+Edwinlo98@users.noreply.github.com>
2019-05-01 22:49:19 +0700

    Commit PCS_Project

---
+++ Proyek PCS/FormLogin.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.DataAccess.Client;
+
+namespace Proyek_PCS
+{
+    public partial class FormLogin : Form
+    {
+        OracleConnection conn;
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OracleConnectionStringBuilder sb = new OracleConnectionStringBuilder();
+            sb.DataSource = textBox1.Text;
+            sb.UserID = textBox2.Text;
+            sb.Password = textBox3.Text;
+            conn = new OracleConnection(sb.ToString());
+            try
+            {
+                if (conn.State==ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                conn.Open();
+                //this.Hide(); new FormPilihCabang().ShowDialog();
+                conn.Close();
+                //this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
